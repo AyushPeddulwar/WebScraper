ada087daf95bb3b530d2c5a83f405f7fc649ad49
AyushPeddulwar <ayushbhaw@gmail.com>
2024-09-06 04:37:01 +0530

    Arguments added

--- scrape.py
+++ scrape.py
@@ -10,4 +10,15 @@ if(platform.system() == 'Windows'):
 if (platform.system() == 'Linux'):
     os.system('clear')
 
+argp = argparse.ArgumentParser(usage = "scraper.py -t TYPE -q QUERY -c [COUNT]")
+argp.add_argument("-t","--type",required= True)
+argp.add_argument("-q","--query",required= True)
+argp.add_argument("-c","--count")
+argp.add_argument("-o","--output")
+parser = argp.parse_args()
+type = parser.type
+query = parser.query
+count = parser.count
+output = parser.output
+if(count == None): count = '10'
 

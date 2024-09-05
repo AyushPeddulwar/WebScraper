e14b65b4c2af19640cd5111a3e8db3c2bf8cbaf9
AyushPeddulwar <ayushbhaw@gmail.com>
2024-09-06 04:20:35 +0530

    Updated

--- scrape.py
+++ scrape.py
@@ -1,1 +1,13 @@
-print ("hello world")
+from bs4 import BeautifulSoup
+import requests, urllib, os, json, argparse
+import platform, datetime
+
+op = ''
+desc = "Articles scraper"
+
+if(platform.system() == 'Windows'):
+    os.system('cls')
+if (platform.system() == 'Linux'):
+    os.system('clear')
+
+
